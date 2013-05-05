7f4995499caa661e039991881d81c07f8f36d1a4
westhood <westhoods@gmail.com>
2013-05-06 00:04:49 +0800

    Follow PEP8 and use dict comprehension

--- const.py
+++ const.py
@@ -1,40 +1,40 @@
-import runtime 
-import operator 
+import runtime
+import operator
 
 __all__ = [
-"OpRet", 
-"OpLoad", 
-"OpLoadLocal", 
-"OpBinOp", 
-"OpCall", 
-"OpJump", 
-"OpTest", 
-"OpLoadConst", 
-"OpTailCall", 
-"OpPop", 
-"OpUnOp", 
-"OpLoadVarg",
-"OpSetLocal", 
-"OpLoadLocal", 
-"OpLoadGlobal", 
-"OpSetGlobal",
-"OpBuildClosure", 
-"OpLoadUpvar", 
-"OpCloseUpvar",
-"OpSetUpvar",
-"OpBuildClosure",
-"OpBuildContinuation",
-"OpHalt",
-"VMOps",
-"BinOps",
-"UnOps",
-"Symbol2UnOp",
-"Symbol2BinOp"
+    "OpRet",
+    "OpLoad",
+    "OpLoadLocal",
+    "OpBinOp",
+    "OpCall",
+    "OpJump",
+    "OpTest",
+    "OpLoadConst",
+    "OpTailCall",
+    "OpPop",
+    "OpUnOp",
+    "OpLoadVarg",
+    "OpSetLocal",
+    "OpLoadLocal",
+    "OpLoadGlobal",
+    "OpSetGlobal",
+    "OpBuildClosure",
+    "OpLoadUpvar",
+    "OpCloseUpvar",
+    "OpSetUpvar",
+    "OpBuildClosure",
+    "OpBuildContinuation",
+    "OpHalt",
+    "VMOps",
+    "BinOps",
+    "UnOps",
+    "Symbol2UnOp",
+    "Symbol2BinOp"
 ]
 
 OpRet = 1
 OpLoad = 2
-OpLoadLocal = 3 
+OpLoadLocal = 3
 OpBinOp = 4
 OpCall = 5
 OpJump = 6
@@ -56,26 +56,24 @@ OpBuildContinuation = 21
 OpHalt = 22
 
 
-VMOps = dict([ (v,k) for k,v in globals().copy().items() if k.startswith("Op") ])
+VMOps = {v: k for k, v in globals().items() if k.startswith("Op")}
 
 BinOps = dict([
-        (1, ("+", operator.add)),
-        (2, ("-", operator.sub)),
-        (3, ("*", operator.mul)),
-        (4, ("/", operator.div)),
-        (5, ("cons", runtime.cons)),
-        (6, ("=", operator.eq)),
-        (7, (">", operator.gt)),
-        ])
+    (1, ("+", operator.add)),
+    (2, ("-", operator.sub)),
+    (3, ("*", operator.mul)),
+    (4, ("/", operator.div)),
+    (5, ("cons", runtime.cons)),
+    (6, ("=", operator.eq)),
+    (7, (">", operator.gt)),
+])
 
-Symbol2BinOp = dict([ (v[0], k) for k,v in BinOps.items() ])
+Symbol2BinOp = {v[0]: k for k, v in BinOps.items()}
 
 UnOps = dict([
-        (1, ("-", operator.neg)),
-        (2, ("car", runtime.car)),
-        (3, ("cdr", runtime.cdr)),
-        ])
+    (1, ("-", operator.neg)),
+    (2, ("car", runtime.car)),
+    (3, ("cdr", runtime.cdr)),
+])
 
-Symbol2UnOp = dict([ (v[0], k) for k,v in UnOps.items() ])
-
-
+Symbol2UnOp = {v[0]: k for k, v in UnOps.items()}
