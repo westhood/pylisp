23aa400fcf186a851381de5d52e74901a751ea43
westhood <westhoods@gmail.com>
2013-05-05 23:58:23 +0800

    Refactoring unit tests
    1. Add run_doc to run lisp script from docstring
    2. Remove obsolete tests

--- tests.py
+++ tests.py
@@ -2,9 +2,9 @@ import unittest
 import parser
 import code
 import vm
-import pdb
 import cStringIO
 import runtime
+import functools
 
 
 def compile(source):
@@ -16,21 +16,59 @@ def compile(source):
     consts = code.generate(ast)
     return consts
 
-class TestPyLang(unittest.TestCase):
+
+class run_doc(object):
+    def __init__(self, results=None):
+        self.results = results
+
+    def __call__(self, func):
+        results = self.results
+
+        @functools.wraps(func)
+        def wrapper(self, *args, **kwds):
+            self.run_script(func.__doc__)
+            if results:
+                self.check_results(results)
+
+        return wrapper
+
+
+class Lisp(unittest.TestCase):
     def check_results(self, expects):
         self.outputs.seek(0)
-        results = [ line.strip() for line in self.outputs.readlines() ]
+        results = [line.strip() for line in self.outputs.readlines()]
         self.assertEqual(len(expects), len(results))
         for result, expect in zip(results, expects):
             self.assertEqual(result, expect)
 
+    def run_script(self, s, debug=False):
+        consts = compile(s)
+
+        m = vm.VM(consts, self.env)
+
+        if debug:
+            def debug_on():
+                m.turn_debug(True)
+                return [None]
+
+            def debug_off():
+                m.turn_debug(False)
+                return [None]
+
+            self.env["debug_on"] = debug_on
+            self.env["debug_off"] = debug_off
+
+        m.start()
+
     def setUp(self):
         outputs = cStringIO.StringIO()
+
         def display(*args):
             for arg in args:
-                print >>outputs, arg,
-            print >>outputs
+                outputs.write(str(arg))
+            outputs.write("\n")
             return [None]
+
         env = {}
 
         def assertEqual(*args):
@@ -44,67 +82,58 @@ class TestPyLang(unittest.TestCase):
         env["display"] = display
         env["assert"] = assertEqual
         env["list"] = newList
+
         self.env = env
         self.outputs = outputs
 
+    @run_doc()
     def test_if(self):
-        s = """
-        (define (test a) (if (= 0 a) (* 2 3) (+ 2 3)))
-        (display (test 0))
-        (display (test 2))
         """
-        consts = compile(s)
-        vm.VM(consts, self.env).start()
-        self.check_results(["6", "5"])
+        (define (test a) (if (= 0 a) (* 2 3) (+ 2 3)))
+        (assert (test 0) 6)
+        (assert (test 2) 5)
+        """
 
+    @run_doc(["(1 2 3)"])
     def test_vargs(self):
-        s = """
+        """
         (define (x . a) (display a))
         (x 1 2 3)
         """
-        consts = compile(s)
-        vm.VM(consts, self.env).start()
-        self.check_results(["(1 2 3)"])
 
+    @run_doc()
     def test_begin(self):
-        s = """
-        (define (main)
-          (begin (define x 1)
-              (display x)
-              x))
-        (main)
         """
-        consts = compile(s)
-        vm.VM(consts, self.env).start()
-        self.check_results(["1"])
+        (define (main)
+          (begin
+              (define x 1)
+              x))
+        (assert (main) 1)
+        """
 
+    @run_doc(["1", "2", "((1 . 2) 1 . 2)"])
     def test_cons(self):
-        s = """
+        """
         (define x (cons 1 2))
         (display (car x))
         (display (cdr x))
         (display (cons x x))
         """
-        consts = compile(s)
-        vm.VM(consts, self.env).start()
-        self.check_results(["1", "2", "((1 . 2) 1 . 2)"])
 
+    @run_doc(["3", "7"])
     def test_let(self):
-        s = """ 
-        (define (main) 
+        """
+        (define (main)
             (let ((x 1) (y 2))
-                (begin 
+                (begin
                     (display (+ x y))
                     (lambda () (+ x 1))))
             (let ((x 3) (y 4))
-                (begin 
+                (begin
                     (display (+ x y))
                     y)))
-        (display (main)) 
+        (assert (main) 4)
         """
-        consts = compile(s)
-        vm.VM(consts, self.env).start()
-        self.check_results(["3", "7", "4"])
 
     def test_closure(self):
         s = """
@@ -112,56 +141,49 @@ class TestPyLang(unittest.TestCase):
         (define (muln n) (lambda (x) (mul x n)))
         (define mul2 (muln 2))
         (define mul3 (muln 3))
-        (display (mul2 2))
-        (display (mul3 3))
-        (display (mul3 3.3))
+        (debug_on)
+        (assert (mul2 2) 4)
+        (debug_off)
+        (assert (mul3 3) 9)
+        (assert (mul3 3.3) (* 3 3.3))
         """
-        consts = compile(s)
-        vm.VM(consts, self.env).start()
-        self.check_results(["4", "9", "9.9"])
+        self.run_script(s, debug=True)
 
-    
+    @run_doc()
     def test_cc(self):
-        s = """
+        """
         (define (func x)
-            (call/cc 
+            (call/cc
                 (lambda (f)
                     (if x (f 2) 3))))
         (assert (func 1) 2)
-        (assert  (func 0) 3)
+        (assert (func 0) 3)
         """
-        consts = compile(s)
-        vm.VM(consts, self.env).start()
 
+    @run_doc(["(4 1 2 3)"])
     def test_uparforvarg(self):
-        s = """
+        """
         (define (add . a) (lambda (b) (cons b a)))
         (define add3 (add 1 2 3))
         (display (add3 4))
         """
-        consts = compile(s)
-        vm.VM(consts, self.env).start()
-        self.check_results(["(4 1 2 3)"])
 
+    @run_doc()
     def test_binops(self):
-        s = """
+        """
         (assert 3 (+ 1 2))
         (assert 2 (* 1 2))
         """
-        consts = compile(s)
-        vm.VM(consts, self.env).start()
-        print self.outputs.getvalue()
-    
+
+    @run_doc()
     def test_py_closure(self):
-        s = """
+        """
         (assert (list 1 2 3) (list 1 2 3))
         """
-        consts = compile(s)
-        vm.VM(consts, self.env).start()
 
+    @run_doc()
     def test_tail_call(self):
-
-        s = """
+        """
         (define (sum from to)
             (begin
                 (define (iter from to acc)
@@ -171,56 +193,6 @@ class TestPyLang(unittest.TestCase):
                 (iter from to 0)))
         (assert (sum 1 100000) 5000050000)
         """
-        consts = compile(s)
-        vm.VM(consts, self.env).start()
-
-     
-def test():
-    s = """
-    (define (sum from to)
-        (begin
-            (define (iter from to acc)
-                (if (> from to)
-                    acc
-                    (iter (+ from 1) to (+ acc from))))
-            (iter from to 0)))
-    (assert (sum 1 1000) 500500)
-    """
-    s = """
-    (define (main) 
-            (let ((x 1) (y 2))
-                (begin 
-                    (display (+ x y))
-                    (lambda () (+ x 1))))
-            (let ((x 3) (y 4))
-                (begin 
-                    (display (+ x y))
-                    y)))
-    """
- 
-    def display(*args):
-        for arg in args:
-            print arg,
-        print ""
-        return [None]
-
-    def newList(*args):
-        ret = runtime.build_list(args)
-        return [ret]
-
-    env = {}
-    env["display"] = display
-    env["list"] = newList 
-
-    s = cStringIO.StringIO(s)
-    lex = parser.Lexer(s)
-    visitor = parser.Transform()
-    ast = parser.exprs(lex)
-    parser.traverse(ast, visitor)
-    print ast
-    consts = code.generate(ast)
-
-    vm.VM(consts, env).start() 
 
 if __name__ == '__main__':
     unittest.main()
